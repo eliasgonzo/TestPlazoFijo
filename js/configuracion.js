a84dcceb32d70770607672365f03adcaf665be77
eliasgonzo <eliasrubengonzalez@hotmail.com>
2017-07-06 19:10:19 -0300

    Add files via upload

---
+++ js/configuracion.js
@@ -0,0 +1,21 @@
+var plazoFijo = [];
+
+	plazoFijo['icbc'] = [];
+	plazoFijo['icbc']['banco'] = 'ICBC';
+	plazoFijo['icbc']['taza'] = '0.1';	
+	
+	plazoFijo['santander'] = [];
+	plazoFijo['santander']['banco'] = 'Santander';
+	plazoFijo['santander']['taza'] = '0.15';	
+	
+	plazoFijo['itau'] = [];
+	plazoFijo['itau']['banco'] = 'Itau';
+	plazoFijo['itau']['taza'] = '0.08';	
+	
+	plazoFijo['macro'] = [];
+	plazoFijo['macro']['banco'] = 'Macro';
+	plazoFijo['macro']['taza'] = '0.11';	
+	
+	plazoFijo['provincia'] = [];
+	plazoFijo['provincia']['banco'] = 'Provincia';
+	plazoFijo['provincia']['taza'] = '0.13';
